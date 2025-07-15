8e97a9fdeace915f3e4edc0f9212004e318a4b2b
0xcreator <shreeharan2003@gmail.com>
2025-07-15 19:36:26 +0530

    feat: :sparkles: ignite-solana-starter template

---
+++ ignite-solana-starter/src/app/_layout.tsx
@@ -0,0 +1,89 @@
+import { useEffect, useState } from "react"
+import { Slot, SplashScreen } from "expo-router"
+import { useFonts } from "@expo-google-fonts/space-grotesk"
+import { KeyboardProvider } from "react-native-keyboard-controller"
+import { initialWindowMetrics, SafeAreaProvider } from "react-native-safe-area-context"
+import { Toaster } from 'sonner-native';
+import { GestureHandlerRootView } from "react-native-gesture-handler"
+
+import { initI18n } from "@/i18n"
+import { ThemeProvider } from "@/theme/context"
+import { customFontsToLoad } from "@/theme/typography"
+import { loadDateFnsLocale } from "@/utils/formatDate"
+import { store } from "@/store/store"
+
+// Solana polyfill
+import { getRandomValues as expoCryptoGetRandomValues } from "expo-crypto";
+import { Buffer } from "buffer";
+import { Provider } from "react-redux"
+global.Buffer = Buffer;
+
+// getRandomValues polyfill
+class Crypto {
+  getRandomValues = expoCryptoGetRandomValues;
+}
+
+const webCrypto = typeof crypto !== "undefined" ? crypto : new Crypto();
+
+(() => {
+  if (typeof crypto === "undefined") {
+    Object.defineProperty(window, "crypto", {
+      configurable: true,
+      enumerable: true,
+      get: () => webCrypto,
+    });
+  }
+})();
+
+SplashScreen.preventAutoHideAsync()
+
+if (__DEV__) {
+  // Load Reactotron configuration in development. We don't want to
+  // include this in our production bundle, so we are using `if (__DEV__)`
+  // to only execute this in development.
+  require("src/devtools/ReactotronConfig.ts")
+}
+
+export { ErrorBoundary } from "@/components/ErrorBoundary/ErrorBoundary"
+
+export default function Root() {
+  const [fontsLoaded, fontError] = useFonts(customFontsToLoad)
+  const [isI18nInitialized, setIsI18nInitialized] = useState(false)
+
+  useEffect(() => {
+    initI18n()
+      .then(() => setIsI18nInitialized(true))
+      .then(() => loadDateFnsLocale())
+  }, [])
+
+  const loaded = fontsLoaded && isI18nInitialized
+
+  useEffect(() => {
+    if (fontError) throw fontError
+  }, [fontError])
+
+  useEffect(() => {
+    if (loaded) {
+      SplashScreen.hideAsync()
+    }
+  }, [loaded])
+
+  if (!loaded) {
+    return null
+  }
+
+  return (
+    <SafeAreaProvider initialMetrics={initialWindowMetrics}>
+      <Provider store={store}>
+        <ThemeProvider>
+          <KeyboardProvider>
+            <GestureHandlerRootView>
+              <Slot />
+              <Toaster position="top-center" richColors={true} />
+            </GestureHandlerRootView>
+          </KeyboardProvider>
+        </ThemeProvider>
+      </Provider>
+    </SafeAreaProvider>
+  )
+}
---
+++ ignite-solana-starter/src/screens/HomeScreen.tsx
@@ -0,0 +1,141 @@
+import React, { FC } from "react"
+import { Text, View, Image, Pressable } from "react-native"
+import { Screen } from "@/components/Screen"
+import { $styles } from "@/theme/styles"
+import { useAppDispatch, useAppSelector } from "@/store/store"
+import { clearAuthDetails, setAuthDetails } from "@/store/features/authSlice"
+import { useAppTheme } from "@/theme/context"
+import type { ThemedStyle } from "@/theme/types"
+import type { ViewStyle, TextStyle } from "react-native"
+import { WalletDetails } from "@/components/WalletDetails"
+import { ToggleTheme } from "@/components/ThemeToggle"
+import { Button } from "@/components/Button"
+import { toast } from "sonner-native"
+import { connectWallet, disconnectWallet, signMessage } from "@/utils/walletUtils"
+
+export const HomeScreen: FC = function WelcomeScreen() {
+
+  const authDetails = useAppSelector((state) => state.auth)
+  const dispatch = useAppDispatch()
+  const { themed } = useAppTheme()
+
+  const handleConnectWallet = async () => {
+    try {
+      const result = await connectWallet()
+      dispatch(setAuthDetails({
+        authToken: result.authToken,
+        address: result.address,
+        walletType: result.walletType
+      }))
+    } catch (err: any) {
+      console.error("error: Connect wallet error", err)
+    }
+  }
+
+  const handleDisconnectWallet = async () => {
+    if (!authDetails.authToken) {
+      console.error("error: No auth token found")
+      return
+    }
+    
+    try {
+      await disconnectWallet(authDetails.authToken)
+      dispatch(clearAuthDetails())
+    } catch (error: any) {
+      console.error("error: Disconnect wallet error", error)
+    }
+  }
+
+  const handleSignMessage = async () => {
+    if (!authDetails.authToken || !authDetails.address) {
+      console.error("error: No auth token or address found")
+      return
+    }
+    
+    try {
+      await signMessage(authDetails.authToken, authDetails.address)
+      toast.success("Message signed successfully")
+    } catch (error: any) {
+      console.error("error: Sign Message Error", error)
+    }
+  }
+
+  return (
+    <Screen preset="fixed" contentContainerStyle={$styles.flex1}>
+      <View style={themed($header)}>
+        <Image source={require("../../assets/icons/logo/solana.png")} />
+        <Text style={themed($headerTitle)}>Ignite Solana Starter</Text>
+        <Text style={themed($headerSubtitle)}>Example design to with MWA support</Text>
+      </View>
+      <View style={themed($mainContent)}>
+        {authDetails.address ? (
+          <View style={themed($innerContent)}>
+            <WalletDetails address={authDetails.address || ""} walletType={authDetails.walletType || ""} onSignMessage={handleSignMessage} />
+            <ToggleTheme />
+          </View>
+        ) : null}
+      </View>
+      <View style={themed($footer)}>
+        {authDetails.address ? 
+          <Button onPress={handleDisconnectWallet} text="Disconnect Wallet" textStyle={themed($connectButtonText)} style={themed($connectButton)} /> : 
+          <Button onPress={handleConnectWallet} text="Connect Wallet" textStyle={themed($connectButtonText)} style={themed($connectButton)} />}
+      </View>
+    </Screen>
+  )
+}
+
+const $header: ThemedStyle<ViewStyle> = (theme) => ({
+  flex: 0.4,
+  justifyContent: "flex-end",
+  alignItems: "center",
+  paddingBottom: theme.spacing.lg,
+})
+
+const $headerTitle: ThemedStyle<TextStyle> = (theme) => ({
+  fontSize: 20,
+  fontFamily: theme.typography.primary.bold,
+  color: theme.colors.text,
+  marginTop: theme.spacing.sm,
+})
+
+const $headerSubtitle: ThemedStyle<TextStyle> = (theme) => ({
+  fontSize: 12,
+  fontFamily: theme.typography.primary.normal,
+  color: theme.colors.textDim,
+})
+
+const $mainContent: ThemedStyle<ViewStyle> = (theme) => ({
+  flex: 0.5,
+  alignItems: "center",
+  padding: theme.spacing.md,
+  marginTop: theme.spacing.lg,
+  gap: 8,
+})
+
+const $innerContent: ThemedStyle<ViewStyle> = (theme) => ({
+  width: "100%",
+  alignItems: "center",
+  gap: theme.spacing.md,
+})
+
+const $footer: ThemedStyle<ViewStyle> = (theme) => ({
+  flex: 0.1,
+  justifyContent: "center",
+  alignItems: "center",
+  padding: theme.spacing.md,
+})
+
+const $connectButton: ThemedStyle<ViewStyle> = (theme) => ({
+  width: '100%',
+  backgroundColor: theme.colors.palette.neutral900,
+  borderRadius: 32,
+  padding: 12,
+  alignItems: "center",
+  justifyContent: "center",
+})
+
+const $connectButtonText: ThemedStyle<TextStyle> = (theme) => ({
+  color: theme.colors.palette.neutral100,
+  fontSize: 16,
+  fontFamily: theme.typography.primary.bold,
+})
