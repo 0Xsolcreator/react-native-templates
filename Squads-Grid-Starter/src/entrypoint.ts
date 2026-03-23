cd9d5f517bdb321a7666dc0b55b11f9a0093b9bf
0Xsolcreator <shreeharan2003@gmail.com>
2026-03-23 19:17:28 +0530

    chore: :wrench: update entrypoint

---
+++ Squads-Grid-Starter/src/entrypoint.ts
@@ -0,0 +1,9 @@
+import "text-encoding"
+import "react-native-get-random-values"
+
+import { Buffer } from "buffer"
+global.Buffer = Buffer
+
+import "@ethersproject/shims"
+
+import "expo-router/entry"
